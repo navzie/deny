4108885cbc531779ea037011bd07bcd84abece96
navzie <132859146+navzie@users.noreply.github.com>
2023-05-08 07:59:58 +0400

    changed full code based on form

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ document.getElementById('waitlist-form').addEventListener('submit', async (event
   event.preventDefault();
 
   const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
   const response = await fetch(event.target.action, {
     method: 'POST',
-    body: formData,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ parameter: data }),
   });
 
   if (response.ok) {
@@ -12,6 +17,7 @@ document.getElementById('waitlist-form').addEventListener('submit', async (event
     if (result.result === 'success') {
       // Display success message
       alert('Thank you for joining the waitlist!');
+      window.location.href = 'https://tally.so/r/wQM2QG';
     } else {
       // Display an error message
       alert('An error occurred. Please try again later.');
